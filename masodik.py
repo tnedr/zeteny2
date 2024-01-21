571a5d800ff308fba750703de2c29a65d2cadd7a
tnedr <tamasnagy1976@gmail.com>
2024-01-21 14:36:26 +0100

    csa

--- masodik.py
+++ masodik.py
@@ -4,8 +4,20 @@
 
 
 #valtozo print
-a = 1
-print(a)
-a=a+1
-print(a)
-print("hello")
+a = 14567
+b = 234567
+
+c = a
+a = b
+b = c
+
+print(a) #ki irom az a -t
+print(a,b)
+print("a valtozo:",a,", b valtozo",b,"c valtozo:",c)
+# a=a+1
+# print(a)
+# print("hello")
+#piros gomb= break point:debugba futtatom a kodot
+#print("a valtozo:",a,", b valtozo",b,"c valtozo:",c) ez azt csinalja hogy:le irtaja alurra a
+#a cuccosokat
+
