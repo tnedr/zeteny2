707abafdcf03d3b4cc71711aa0438896892d39a4
tnedr <tamasnagy1976@gmail.com>
2024-01-21 14:15:44 +0100

    csa

---
+++ masodik.py
@@ -0,0 +1,11 @@
+#commit gepen levo kodot bele tesszuk a repoba (git:commit)
+#push be kommitolt kodot be rakja a githubra (git:push)
+#update gepen levo kodot frissiti a githubon levo kodra
+
+
+#valtozo print
+a = 1
+print(a)
+a=a+1
+print(a)
+print("hello")
