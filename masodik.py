acab40c982ed5cd15b8f1b6328ee95ccd17fecac
tnedr <tamasnagy1976@gmail.com>
2024-01-27 15:41:34 +0100

    csa

--- masodik.py
+++ masodik.py
@@ -4,16 +4,18 @@
 
 
 #valtozo print
-a = 14567
-b = 234567
+#a = 14567
+# b = 234567
+#
+# c = a
+# a = b
+# b = c
+#
+# print(a) #ki irom az a -t
+# print(a,b)
+# print("a valtozo:",a,", b valtozo",b,"c valtozo:",c)
 
-c = a
-a = b
-b = c
 
-print(a) #ki irom az a -t
-print(a,b)
-print("a valtozo:",a,", b valtozo",b,"c valtozo:",c)
 
 # a=a+1
 # print(a)
@@ -21,4 +23,38 @@ print("a valtozo:",a,", b valtozo",b,"c valtozo:",c)
 #piros gomb= break point:debugba futtatom a kodot
 #print("a valtozo:",a,", b valtozo",b,"c valtozo:",c) ez azt csinalja hogy:le irtaja alurra a
 #a cuccosokat
+def kiir_szamok():
+    for i in range(1, 101):
+        print(i)
 
+#kiir_szamok()
+
+# print("a változó:", a, ", b változó", b, "c változó:", c)
+# szamok = [1, 2, 3, 4, 5]
+# for szam in szamok:
+#     print(szam)
+#  szamok = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+#  for szam in szamok:
+#      print(szam)
+i = 1
+# while i <= 100:
+#     print(i)
+#     i += 1
+# for i in range (10):
+#     print(wasd)
+# /for i in range(10):
+#     print("wasd")
+# i = 0
+# while i < 100:
+#     print("nem tudom")
+#     i += 1kiir_szamok()
+#     osszeg = 0
+#     for szam in range(1, 11):
+#         osszeg += szam
+#
+#     print("Az első 10 pozitív egész szám összege:", osszeg)
+osszeg = 0
+for szam in range(1, 11):
+    osszeg += szam
+
+print("Az első 10 pozitív egész szám összege:", osszeg)
